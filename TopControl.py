55de0a602fd6c52476e61ec16c11439c05997c4a
周宇盛 Jason Zhou <jasonzhouu@gmail.com>
2018-05-22 23:14:49 +0800

    测试成功过

--- TopControl.py
+++ TopControl.py
@@ -10,15 +10,17 @@ import jieba.analyse
 from sklearn.metrics.pairwise import cosine_similarity
 
 if __name__ == '__main__':
+    print('\n======================== \n  start analysing ...\n======================== \n')
+
     # initialize jieba
     jieba.analyse.set_stop_words("stopwords.txt")
 
     my_engine = ContentEngine('localhost', 'root', 'root', 'rss')
 
-    # read updated news from database
+    # read uddated news from database
     now_date = datetime.datetime.now().strftime("%Y-%m-%d")
     # only analyse article get today
-    sql = "SELECT id, title, content FROM articles WHERE created_at LIKE " + "'" + now_date + "%';"
+    sql = "SELECT id, title, content FROM articles"
     lines = my_engine.execute_sql(sql)
 
     update_news = pd.DataFrame()  # store update news
@@ -27,6 +29,7 @@ if __name__ == '__main__':
         clean_content = my_engine.clean_content(line[2])
 
         one_news = pd.DataFrame({'newsid': str(line[0]), 'title': line[1], 'content': clean_content}, index=[0])
+        # print(one_news)
         update_news = update_news.append(one_news, ignore_index=True)
 
     # convert news to vector
@@ -36,6 +39,7 @@ if __name__ == '__main__':
         one_title_vector = my_engine.get_news_vector(update_news.title[i])
         one_news_vector = my_engine.get_news_vector(update_news.content[i])
         news_vector[news_id] = one_title_vector + one_news_vector
+        print('news vector', news_vector[news_id])
 
     # read all user id
     sql = "SELECT id FROM users;"
@@ -43,50 +47,50 @@ if __name__ == '__main__':
     user_list = [str(user_id[0]) for user_id in user_tuple]  # user id list
 
     # update user interesting model and recommend news
-    for user in user_list:
-        # read the latest 50 recordings
-        sql = "SELECT article_id FROM reading_history_articles" + " WHERE user_id=" + user + " ORDER BY created_at DESC LIMIT 50;"
-        rcd_tuple = my_engine.execute_sql(sql)
-        rcd_list = [str(rcd[0]) for rcd in rcd_tuple]  # recording id list
-        if not rcd_list:
-            # if no recordings, continue
-            continue;
+    # read the latest 50 recordings
+    sql = "SELECT article_id FROM reading_history_articles WHERE user_id=1 ORDER BY created_at DESC LIMIT 50;"
+    rcd_tuple = my_engine.execute_sql(sql)
+    rcd_list = [str(rcd[0]) for rcd in rcd_tuple]  # recording id list
+    # if not rcd_list:
+    #     # if no recordings, continue
+    #     continue;
 
-        # compute eim for each user
-        user_eim = np.zeros(len(my_engine.feature_sequence))
-        for rcd in rcd_list:
-            sql = "SELECT title, content FROM articles WHERE id=" + rcd + ";"
-            news = my_engine.execute_sql(sql)
-            news_title = news[0][0]
-            news_content = my_engine.clean_content(news[0][1])
+    # compute eim for each user
+    user_eim = np.zeros(len(my_engine.feature_sequence))
+    for rcd in rcd_list:
+        print('article history: ', rcd)
+        sql = "SELECT title, content FROM articles WHERE id=" + rcd + ";"
+        news = my_engine.execute_sql(sql)
+        news_title = news[0][0]
+        news_content = my_engine.clean_content(news[0][1])
 
-            content_vector = my_engine.get_news_vector(news_content)
-            title_vector = my_engine.get_news_vector(news_title)
-            user_eim += content_vector + title_vector
+        content_vector = my_engine.get_news_vector(news_content)
+        title_vector = my_engine.get_news_vector(news_title)
+        user_eim += content_vector + title_vector
 
-        user_eim = user_eim / len(rcd_list)
+    user_eim = user_eim / len(rcd_list)
 
-        # recommend news
-        recommend_result = pd.DataFrame(columns=['newsid', 'similarity'])
+    # recommend news
+    recommend_result = pd.DataFrame(columns=['newsid', 'similarity'])
 
-        for newsid, one_news_vector in news_vector.items():
-            similarity = cosine_similarity(user_eim[np.newaxis, :],
-                                            one_news_vector[np.newaxis, :])
-            one_result = pd.DataFrame({'newsid': newsid,
-                                        'similarity': similarity[0][0]},
-                                        index=[0])
-            recommend_result = recommend_result.append(one_result, ignore_index=True)
+    for newsid, one_news_vector in news_vector.items():
+        similarity = cosine_similarity(user_eim[np.newaxis, :],
+                                        one_news_vector[np.newaxis, :])
+        one_result = pd.DataFrame({'newsid': newsid,
+                                    'similarity': similarity[0][0]},
+                                    index=[0])
+        recommend_result = recommend_result.append(one_result, ignore_index=True)
 
-        recommend_result = recommend_result.sort_values(by='similarity', ascending=False)
+    recommend_result = recommend_result.sort_values(by='similarity', ascending=False)
 
-        # write recommend result to database
-        for index, row in recommend_result.iterrows():
-            user_id = user
-            article_id = row.newsid
-            similarity = str(row.similarity)
-            created_at = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    # write recommend result to database
+    for index, row in recommend_result.iterrows():
+        user_id = "1"
+        article_id = row.newsid
+        similarity = str(row.similarity)
+        created_at = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-            sql = "INSERT INTO recommend_articles (article_id, user_id, similarity, created_at)" + " VALUES " + "('" + article_id + "', '" + user_id + "', '" + similarity + "', '" + created_at + "');"
-            my_engine.execute_sql(sql, commit=True)
+        sql = "INSERT INTO recommend_articles (article_id, user_id, similarity, created_at) VALUES ('" + article_id + "', '" + user_id + "', '" + similarity + "', '" + created_at + "');"
+        my_engine.execute_sql(sql, commit=True)
 
-        recommend_result.drop(recommend_result.index, inplace=True)
+    recommend_result.drop(recommend_result.index, inplace=True)
