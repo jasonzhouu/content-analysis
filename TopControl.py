5151bcbb852b100891949a0edf3331408f0d3a3f
周宇盛 Jason Zhou <jasonzhouu@gmail.com>
2018-05-22 11:22:33 +0800

    不需要考虑时间

--- TopControl.py
+++ TopControl.py
@@ -17,104 +17,78 @@ if __name__ == '__main__':
 
     my_engine = ContentEngine('localhost', 'root', 'xiaoyu2698355', 'rss')
 
-    # control the procedure of recommendation
-    work_start = False
-    work_finish = False
-    while True:
-        # check if it's 3:00
-        now_time = datetime.datetime.now().strftime("%H:%M")
+    # read updated news from database
+    now_date = datetime.datetime.now().strftime("%Y-%m-%d")
+    # 只对当天录入数据库的文章分析特征向量
+    sql = "SELECT id, title, content FROM articles WHERE created_at LIKE " + "'" + now_date + "%';"
+    lines = my_engine.execute_sql(sql)
 
-        if now_time == '3:00' and not work_finish:
-            work_start = True
-        else:
-            work_start = False
+    update_news = pd.DataFrame()  # store update news
+    for line in lines:
+        # clean news content
+        clean_content = my_engine.clean_content(line[2])
 
-        # start work
-        if work_start:
-            work_start = False
+        one_news = pd.DataFrame({'newsid': str(line[0]), 'title': line[1], 'content': clean_content}, index=[0])
+        update_news = update_news.append(one_news, ignore_index=True)
 
-            # get news
-            success = execute_js('rssspider.js')
+    # convert news to vector
+    news_vector = dict()  # store updated news vectors
+    for i in update_news.index:
+        news_id = update_news.newsid[i]
+        one_title_vector = my_engine.get_news_vector(update_news.title[i])
+        one_news_vector = my_engine.get_news_vector(update_news.content[i])
+        news_vector[news_id] = one_title_vector + one_news_vector
 
-            # read updated news from database
-            now_date = datetime.datetime.now().strftime("%Y-%m-%d")
-            sql = "SELECT id, title, content FROM articles WHERE created_at LIKE " + "'" + now_date + "%';"
-            lines = my_engine.execute_sql(sql)
+    # read all user id
+    sql = "SELECT id FROM users;"
+    user_tuple = my_engine.execute_sql(sql)
+    user_list = [str(user_id[0]) for user_id in user_tuple]  # user id list
 
-            update_news = pd.DataFrame()  # store update news
-            for line in lines:
-                # clean news content
-                clean_content = my_engine.clean_content(line[2])
+    # update user interesting model and recommend news
+    for user in user_list:
+        # read the latest 50 recordings
+        sql = "SELECT article_id FROM reading_history_articles" + " WHERE user_id=" + user + " ORDER BY created_at DESC LIMIT 50;"
+        rcd_tuple = my_engine.execute_sql(sql)
+        rcd_list = [str(rcd[0]) for rcd in rcd_tuple]  # recording id list
+        if not rcd_list:
+            # if no recordings, continue
+            continue;
 
-                one_news = pd.DataFrame({'newsid': str(line[0]), 'title': line[1], 'content': clean_content}, index=[0])
-                update_news = update_news.append(one_news, ignore_index=True)
+        # compute eim for each user
+        user_eim = np.zeros(len(my_engine.feature_sequence))
+        for rcd in rcd_list:
+            sql = "SELECT title, content FROM articles WHERE id=" + rcd + ";"
+            news = my_engine.execute_sql(sql)
+            news_title = news[0][0]
+            news_content = my_engine.clean_content(news[0][1])
 
-            # convert news to vector
-            news_vector = dict()  # store updated news vectors
-            for i in update_news.index:
-                news_id = update_news.newsid[i]
-                one_title_vector = my_engine.get_news_vector(update_news.title[i])
-                one_news_vector = my_engine.get_news_vector(update_news.content[i])
-                news_vector[news_id] = one_title_vector + one_news_vector
+            content_vector = my_engine.get_news_vector(news_content)
+            title_vector = my_engine.get_news_vector(news_title)
+            user_eim += content_vector + title_vector
 
-            # read all user id
-            sql = "SELECT id FROM users;"
-            user_tuple = my_engine.execute_sql(sql)
-            user_list = [str(user_id[0]) for user_id in user_tuple]  # user id list
+        user_eim = user_eim / len(rcd_list)
 
-            # update user interesting model and recommend news
-            for user in user_list:
-                # read the latest 50 recordings
-                sql = "SELECT article_id FROM reading_history_articles" + " WHERE user_id=" + user + " ORDER BY created_at DESC LIMIT 50;"
-                rcd_tuple = my_engine.execute_sql(sql)
-                rcd_list = [str(rcd[0]) for rcd in rcd_tuple]  # recording id list
-                if not rcd_list:
-                    # if no recordings, continue
-                    continue;
+        # recommend news
+        recommend_result = pd.DataFrame(columns=['newsid', 'similarity'])
 
-                # compute eim for each user
-                user_eim = np.zeros(len(my_engine.feature_sequence))
-                for rcd in rcd_list:
-                    sql = "SELECT title, content FROM articles WHERE id=" + rcd + ";"
-                    news = my_engine.execute_sql(sql)
-                    news_title = news[0][0]
-                    news_content = my_engine.clean_content(news[0][1])
+        for newsid, one_news_vector in news_vector.items():
+            similarity = cosine_similarity(user_eim[np.newaxis, :],
+                                            one_news_vector[np.newaxis, :])
+            one_result = pd.DataFrame({'newsid': newsid,
+                                        'similarity': similarity[0][0]},
+                                        index=[0])
+            recommend_result = recommend_result.append(one_result, ignore_index=True)
 
-                    content_vector = my_engine.get_news_vector(news_content)
-                    title_vector = my_engine.get_news_vector(news_title)
-                    user_eim += content_vector + title_vector
+        recommend_result = recommend_result.sort_values(by='similarity', ascending=False)
 
-                user_eim = user_eim / len(rcd_list)
+        # write recommend result to database
+        for index, row in recommend_result.iterrows():
+            user_id = user
+            article_id = row.newsid
+            similarity = str(row.similarity)
+            created_at = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-                # recommend news
-                recommend_result = pd.DataFrame(columns=['newsid', 'similarity'])
+            sql = "INSERT INTO recommend_articles (article_id, user_id, similarity, created_at)" + " VALUES " + "('" + article_id + "', '" + user_id + "', '" + similarity + "', '" + created_at + "');"
+            my_engine.execute_sql(sql, commit=True)
 
-                for newsid, one_news_vector in news_vector.items():
-                    similarity = cosine_similarity(user_eim[np.newaxis, :],
-                                                   one_news_vector[np.newaxis, :])
-                    one_result = pd.DataFrame({'newsid': newsid,
-                                               'similarity': similarity[0][0]},
-                                              index=[0])
-                    recommend_result = recommend_result.append(one_result, ignore_index=True)
-
-                recommend_result = recommend_result.sort_values(by='similarity', ascending=False)
-
-                # write recommend result to database
-                for index, row in recommend_result.iterrows():
-                    user_id = user
-                    article_id = row.newsid
-                    similarity = str(row.similarity)
-                    created_at = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-                    sql = "INSERT INTO recommend_articles (article_id, user_id, similarity, created_at)" + " VALUES " + "('" + article_id + "', '" + user_id + "', '" + similarity + "', '" + created_at + "');"
-                    my_engine.execute_sql(sql, commit=True)
-
-                recommend_result.drop(recommend_result.index, inplace=True)
-
-            # when recommend for all users finished
-            work_finish = True
-
-        # when work finish
-        now_time = datetime.datetime.now().strftime("%H:%M")
-        if now_time != '3:00':
-            work_finish = False
+        recommend_result.drop(recommend_result.index, inplace=True)
